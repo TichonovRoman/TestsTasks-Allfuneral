f81534cb7ddb9f0c13d427dba83b8f27b111e301
TichonovRoman <romikt@inbox.ru>
2022-07-09 00:38:54 +0300

    fix env

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiUk9NQSIsImlhdCI6MTY1NzEzNjcwNSwiZXhwIjoxNjU3NzQxNTA1fQ.dYiXmvuNjXkxPpkSCu9kvKP4J1zvxol4r8lPKIyCNkg";
-
 export const apiConfig = axios.create({
-    baseURL: "http://135.181.35.61:2112/",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
     }
 });
